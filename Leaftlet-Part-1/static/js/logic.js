5d1f8ff95c2d0e59890babb398ee645cc373b34c
Solarnite <chuthieng@gmail.com>
2023-02-16 22:07:25 -0500

    Finalize maps

--- Leaftlet-Part-1/static/js/logic.js
+++ Leaftlet-Part-1/static/js/logic.js
@@ -1,14 +1,14 @@
 // Earthquakes URL
-const URL = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
+var url = "https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson";
 
-// Request on URL to retrieve earthquakeData.features
-d3.json(URL).then(function(earthquakeData)  {
-    createMaps(earthquakeData.features);
+// Request on URL to retrieve earthquake data.features
+d3.json(url).then(function(data)  {
+    createMaps(data);
 });
 
 function markerSize(magnitude) {
-    return magnitude * 4;
-};
+    return magnitude * 20000;
+}
 
 function markerColor(depth) {
     switch(true) {
@@ -25,7 +25,7 @@ function markerColor(depth) {
         default:
             return "#a3f600";
     };
-};
+}
 
 function createMaps(earthquake) {
     // Creation of tilelayer
@@ -40,56 +40,49 @@ function createMaps(earthquake) {
         layers: street
     });
 
-    // Creation of GeoJSON layer of data
-    let features = earthquakeData.features;
-    let depth_array = [];
-
     // loop
-    for (let i = 0; i < earthquake.length; i++) {
+        for (let i = 0; i < earthquake.features.length; i++) {
+            // Define some variables for Earthquake Data properties 
+            let features = earthquake.features[i];
+            let coordinates = features.geometry.coordinates;
+            let latitude = coordinates[1];
+            let longitude = coordinates[0];
+            let depth = coordinates[2];
 
-        // Define some variables for Earthquake Data properties 
-        let coordinates = features[i].geometry.coordinates;
-        let latitude = coordinates[1];
-        let longitude = coordinates[0];
+            // Define magnitude and place
+            let properties = features.properties;
+            let magnitude = properties.mag;
+            let place = properties.place;
 
-        // Define depth then push to array
-        let depth = coordinates[2];
-        depth_array.push(depth);
+            // Creation of Markers
+            markers = L.circle([latitude, longitude], {
+                radius: markerSize(magnitude),
+                color: "000000",
+                opacity: 0.5,
+                weight: 1,
+                fillColor: markerColor(depth),
+                fillOpacity: 0.5
+            }).bindPopup(`<h1>${place}</h1><br/>Magnitude: ${magnitude}<br/>Depth: ${depth}`).addTo(myMap);
 
-        let properties = features[i].properties;
-
-        // Define magnitude and place
-        let magnitude = properties.mag;
-        let place = properties.place;
-
-        // Creation of Markers
-        markers = L.markerCircle([latitude, longitude], {
-            radius: markerSize(magnitude),
-            color: "000000",
-            opacity: 1,
-            weight: 1,
-            fillColor: markerColor(depth),
-            fillOpacity: 1
-        }).bindPopup(`<h3>${place}</h3><br/>Magnitude: ${magnitude}<br/>Depth: ${depth}`).addTo(myMap);
-
-    }
+        }
 
     // Legend
     var legend = L.control({position: "bottomright"});
 
-    legend.onAdd = function() {
+    legend.onAdd = function(map) {
         let div = L.DomUtil.create("div", "legend");
-        let limits = [-10, 10, 30, 50, 70, 90];
+        let limits = [0, 10, 30, 50, 70, 90];
         div.innerHTML = "<h3 style = 'text-align: center'>Depth in km</h3>"
     
         for (let i = 0; i < limits.length; i++) {
             div.innerHTML += 
             '<i style ="background:' + markerColor(limits[i] + 1) + '"></i> ' +
             limits[i] + (limits[i + 1] ? '&ndash;' + limits[i + 1] + '<br>' : '+');
-        };
+        }
         return div;
     };
 
     // Add legend to map
     legend.addTo(myMap);
 }
+
